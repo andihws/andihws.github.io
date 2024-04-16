faa1ffa7dda9b1c6c709f19ca192491b157fa397
Allison Zhang <139592103+andihws@users.noreply.github.com>
2024-04-16 13:05:47 -0400

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "helloapp",
+  "version": "1.0.0",
+  "description": "node app ",
+  "main": "hello_app.js",
+  "scripts": {
+  "start": "node hello_app.js"
+},
+  "repository": {
+  "type": "git",
+  "url": ""
+},
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+  "url": ""
+},
+  "homepage": ""
+}
